593a6e13dbd0e13e2e95395dac1e8804ffa41f58
Dhruv Parekh <parekhdhruv04@gmail.com>
2023-04-03 13:21:42 +0530

    Add cross-fetch example

---
+++ functions/cross-fetch.js
@@ -0,0 +1,10 @@
+import fetch from 'cross-fetch';
+
+export default async function handler(req, res) {
+    const response = await fetch('https://api.github.com/users/github');
+    const data = await response.json();
+
+    console.log(data);
+
+    res.status(200).send(data);
+}
